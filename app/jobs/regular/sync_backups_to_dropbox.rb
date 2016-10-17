3b3ab75671e77136a10085318a7e3fa9aeb30e3b
Rafael dos Santos Silva <xfalcox@gmail.com>
2016-10-17 14:47:55 -0200

    Change to low queue on sidekiq

--- app/jobs/regular/sync_backups_to_dropbox.rb
+++ app/jobs/regular/sync_backups_to_dropbox.rb
@@ -1,5 +1,8 @@
 module Jobs  
   class SyncBackupsToDropbox < ::Jobs::Base
+
+  	sidekiq_options queue: 'low'
+
     def execute(args)
       ::DiscourseBackupToDropbox::DropboxSynchronizer.sync if SiteSetting.discourse_backups_to_dropbox_enabled
     end
