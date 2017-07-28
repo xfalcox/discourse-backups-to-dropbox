00649c8cdf2196d7d7099bd46f2617cd76c67346
KAJATIGER <ksantro@gmx.de>
2017-07-28 15:40:23 +0200

    trying to get a good subclass running here. having difficulties with the dbx method

--- lib/dropbox_synchronizer.rb
+++ lib/dropbox_synchronizer.rb
@@ -5,12 +5,12 @@ module DiscourseBackupToDropbox
 
     def initialize(backup)
       super(backup) #need to initialize neccessary params for the can_sync?
-      @api_key = SiteSetting.discourse_backups_to_dropbox_api_key
+      @api_key   = SiteSetting.discourse_backups_to_dropbox_api_key
       @turned_on = SiteSetting.discourse_backups_to_dropbox_enabled
     end
 
     def dbx
-      @dbx = Dropbox::Client.new(@api_key)
+      @dbx ||= Dropbox::Client.new(@api_key)
     end
 
     protected
@@ -19,37 +19,36 @@ module DiscourseBackupToDropbox
       @turned_on && @api_key.present? && backup.present?
     end
 
-    def create_folder # this is rather a create and pick folder method
+    def add_to_folder(file) # this is rather a create and pick folder method
       folder_name = Discourse.current_hostname
       begin
         @dbx.create_folder("/#{folder_name}")
       rescue
         #folder already exists
       end
-      dropbox_backup_files = @dbx.list_folder("/#{folder_name}").map(&:name)
+      dropbox_backup_files   = @dbx.list_folder("/#{folder_name}").map(&:name)
     end
 
-    def perform_sync(dropbox_backup_files)
-      local_backup_files     = Backup.all.take(SiteSetting.discourse_backups_to_dropbox_quantity)
-      file_difference_local  = local_backup_files - dropbox_backup_files
+    def perform_sync
       full_path  = backup.path   # took out the loop here
       filename   = backup.filename
       size       = backup.size
-      upload.dbx(folder_name, filename, full_path, size)
+      file       = upload(folder_name, filename, full_path, size)
+
 
       file_difference_remote = dropbox_backup_files - local_backup_files
-      file_difference_remote.dbx.delete("/#{folder_name}/#{filename}") # passed @dbx as a second argument here, dont know if that works
+      file_difference_remote.dbx.delete("/#{folder_name}/#{filename}") # used dbx, as a method here
     end                                                                  # and took out the loop
 
-    def upload(dbx, folder_name, file_name, full_path, size)
+    def upload(folder_name, file_name, full_path, size)
       if size < UPLOAD_MAX_SIZE then
         @dbx.upload("/#{folder_name}/#{file_name}", File.open(full_path, "r"))
       else
-        backup.chunked_upload(@dbx, folder_name, file_name, full_path)
+        backup.chunked_upload(folder_name, file_name, full_path)
       end
     end
 
-    def chunked_upload(dbx, folder_name, file_name, full_path)
+    def chunked_upload(folder_name, file_name, full_path)
       File.open(full_path) do |f|
         loops = f.size / CHUNK_SIZE
 
