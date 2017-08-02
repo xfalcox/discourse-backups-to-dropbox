fb3da5b706a4fbe1c812a5c72a2ac618be38e6bb
Jen <jvarelalijo@gmail.com>
2017-08-02 10:51:06 +0200

    job readability

--- app/jobs/regular/sync_backups_to_dropbox.rb
+++ app/jobs/regular/sync_backups_to_dropbox.rb
@@ -4,7 +4,8 @@ module Jobs
   	sidekiq_options queue: 'low'
 
     def execute(args)
-      Backup.all.take(SiteSetting.discourse_sync_to_dropbox_quantity).each do |backup|
+      backups = Backup.all.take(SiteSetting.discourse_sync_to_dropbox_quantity)
+      backups.each do |backup|
         DiscourseBackupToDropbox::DropboxSynchronizer.new(backup).sync
       end
     end
