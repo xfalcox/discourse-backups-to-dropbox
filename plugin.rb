381d32061af4a5d453e4b0719eececdc3e55d073
Jen <jvarelalijo@gmail.com>
2017-08-02 10:45:06 +0200

    simple uploader

--- plugin.rb
+++ plugin.rb
@@ -6,14 +6,11 @@
 
 gem 'public_suffix', '2.0.5', {require: false }
 gem 'domain_name', '0.5.20170404', {require: false }
-
 gem 'addressable', '2.5.1', {require: false }
 gem 'http_parser.rb', '0.6.0', {require: false }
 gem 'http-cookie', '1.0.3', {require: false }
 gem 'http-form_data', '1.0.1', {require: false }
-
 gem 'http', '2.0.3', {require: false }
-
 gem 'dropbox-sdk-v2', '0.0.3', { require: false }
 
 enabled_site_setting :discourse_sync_to_dropbox_enabled
@@ -21,10 +18,8 @@ enabled_site_setting :discourse_sync_to_dropbox_enabled
 require 'dropbox'
 
 after_initialize do
-
   load File.expand_path("../app/jobs/regular/sync_backups_to_dropbox.rb", __FILE__)
   load File.expand_path("../lib/dropbox_synchronizer.rb", __FILE__)
-
 
   Backup.class_eval do
     def after_create_hook
@@ -32,4 +27,3 @@ after_initialize do
     end
   end
 end
-
